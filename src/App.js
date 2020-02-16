ca703c8e2e52eaa76f2d67d4e2cb3d847a7b157f
unknown <macbaren124@gmail.com>
2020-02-16 18:29:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react'
+import TodoList from './Todo/TodoList'
+import Context from './context'
+
+import Loader from './Loader'
+
+const AddTodo = React.lazy(() => import('./Todo/AddTodo'))
 
 function App() {
+
+  const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+        setTodos(todos)
+        setLoading(false)
+        }, 2000)
+      })
+  }, [])
+
+  function toggled(id) {
+    setTodos(
+      todos.map(todo => {
+        if(id === todo.id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+
+  function removeTodo (id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false
+        }
+      ])
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>React Turtorial</h1>
+        <React.Suspense fallback={<p>...Loading</p>}>
+          <AddTodo onCreate={addTodo}/>
+        </React.Suspense>
+
+        {loading && <Loader />}
+
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggled}/>
+        ) : (
+          loading ? null : <p>No todos</p>
+        )}
+
+      </div>
+    </Context.Provider>
+  )
 }
 
-export default App;
+export default App
